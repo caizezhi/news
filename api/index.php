3bf28eff64363e33efc0bbda1de9ce27903d7230
caosm <csmgoon@163.com>
2015-09-30 23:07:34 +0800

    beta

--- api/index.php
+++ api/index.php
@@ -1,2 +1,37 @@
 <?php
+require 'Slim/Slim.php';
+$app = new Slim();
+
+//For User
+$app->get('/index/news','news');
+
+
+// For Admin
+$app->get('/admin/news','getAllNews');
+
+$app->run();
+
+function MysqlDb()
+{
+	$dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $dbname = "news";
+    $db = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+    $db->query("set names utf8");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $db;
+}
+
+function news()
+{
+}
+
+function getAllNews()
+{
+	$sql = "SELECT * FROM `zx_article` ORDER BY `time` DESC LIMIT 1";
+	$db = MysqlDb();
+	$res = $db->query($sql);
+	echo json_encode($res->fetchAll(PDO::FETCH_OBJ));
+}
 ?>
